7bf3403a34077127a8674ef2d78a35c994a22dcc
sarahdzhu <zhudo@ssfs.org>
2022-02-09 08:58:32 -0500

    donor

--- src/Donor.java
+++ src/Donor.java
@@ -1,4 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Donor {
+
+    private String name;
+    private double amount;
+
+    public Donor(String n, Double amt){
+      this.name=n;
+      this.amount=amt;
+
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 
 
 }
+
+
--- src/DonorRunner.java
+++ src/DonorRunner.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -11,8 +12,8 @@ public class DonorRunner {
         donorList = new ArrayList<>();
         getDonorList("files/donors.txt");
         sortDonorList();
-        //System.out.println(donorList.get(0).getName()); // Should print out Hernandez
-        //System.out.println(donorList.get(donorList.size()-1).getName()); // Should print out Hall
+        System.out.println(donorList.get(0).getName()); // Should print out Hernandez
+        System.out.println(donorList.get(donorList.size()-1).getName()); // Should print out Hall
         System.out.println(getDonatedAmount("Anderson")); // should print 1436.5
         System.out.println(getDonatedAmount("Roberts")); // should print 307.86
         System.out.println(getDonatedAmount("Wilkinson")); // should print 0.0
@@ -20,14 +21,51 @@ public class DonorRunner {
     }
 
     public static void getDonorList(String filename) throws IOException {
+            Scanner input = new Scanner(new File(filename));
 
-    }
+            while (input.hasNext()) {
+                String name= input.next();
+                Double amount= input.nextDouble();
+
+                Donor nextDonor= new Donor(name, amount);
+
+                donorList.add(nextDonor);
+
+            }
+
+            // Your code should go below here, and above the return statement.
+        }
 
     public static void sortDonorList() {
+
+
+        for(int i=0; i<donorList.size()-1; i++ ){
+            int largest= i;
+           for(int j=i+1; j<donorList.size();j++){
+               if(donorList.get(j).getAmount()>donorList.get(largest).getAmount()){
+                   largest=j;
+               }
+               Donor temp=donorList.get(i);
+               donorList.set(i,donorList.get(largest));
+               donorList.set(largest,temp);
+           }
+        }
 
     }
 
     public static double getDonatedAmount(String name) {
+            int min = 0;
+            int max = donorList.size() - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (donorList.get(mid).getName().compareTo(name) == 0)
+                    return 0.0;
+                else if (donorList.get(mid).getName().compareToIgnoreCase(name) > 0)
+                    max = mid - 1;
+                else
+                    min = mid + 1;
+            }
 
 
         return 0.0; // this is returned if the name is not found
